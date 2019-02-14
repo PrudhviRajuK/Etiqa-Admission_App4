fbb6657ce58a30e4e7edfd5f9bcaf2593f369fb3
PrudhviRajuK <47547595+PrudhviRajuK@users.noreply.github.com>
2019-02-14 10:43:00 +0800

    remove unused import

--- src/main/java/com/university/student/DemoApplication.java
+++ src/main/java/com/university/student/DemoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.university.student.controller.StudentController;
 
 @SpringBootApplication(scanBasePackages="com")
 //@ComponentScan(basePackageClasses = StudentCourseController.class)
